937eba729a8d88e404ff7d2ef37888abc69c1b67
Pablo <pablogarcirodriguez@gmail.com>
2024-06-03 12:15:40 +0200

    create routing

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-const miURL = new URL(
-  "https://www.ejemplo.org/cursos/programacion?ordenar=vistas&nivel=1"
-);
+const http = require("http");
 
-console.log(miURL.hostname); // www.ejemplo.org
+const { infoCursos } = require("./cursos");
 
-console.log(miURL.pathname); // /cursos/programacion
+const servidor = http.createServer((req, res) => {
+  const { method: metodo } = req;
 
-console.log(miURL.searchParams); // URLSearchParams { 'ordenar' => 'vistas', 'nivel' => '1' }
-console.log(miURL.searchParams.get("ordenar"));
-console.log(miURL.searchParams.get("nivel"));
+  switch (metodo) {
+    case "GET":
+      return manejarSolicitudGET(req, res);
+    case "POST":
+      return manejarSolicitudPOST(req, res);
+    default:
+      res.statusCode = 501;
+      res.end(
+        `El metodo usado no puede ser manejado por el servidor: ${metodo}`
+      );
+  }
+});
+
+function manejarSolicitudGET(req, res) {
+  const path = req.url;
+
+  console.log(res.statusCode); // 200 ok
+
+  if (path === "/") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    return res.end("Bienvenido a mi primer servidor y API creados con Node.js");
+  } else if (path === "/api/cursos" || path === "/api/cursos/") {
+    return res.end(JSON.stringify(infoCursos));
+  } else if (
+    path === "/api/cursos/programacion" ||
+    path === "/api/cursos/programacion/"
+  ) {
+    return res.end(JSON.stringify(infoCursos.programacion));
+  }
+
+  res.statusCode = 404;
+  res.end("Recurso solicitado no existe");
+}
+
+function manejarSolicitudPOST(req, res) {
+  const path = req.url;
+
+  if (path === "/api/cursos/programacion") {
+    console.log(
+      "El servidor recibio una solicitud POST para cursos/programacion"
+    );
+    return res.end(
+      "El servidor recibio una solicitud POST para cursos/programacion"
+    );
+  }
+}
+
+const PUERTO = 3000;
+
+servidor.listen(PUERTO, () => {
+  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+});
---
+++ cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+  programacion: [
+    {
+      id: 1,
+      titulo: "Aprende python",
+      lenguaje: "python",
+      vistas: 15000,
+      nivel: "basico",
+    },
+    {
+      id: 2,
+      titulo: "Python intermedio",
+      lenguaje: "python",
+      vistas: 13500,
+      nivel: "intermedio",
+    },
+    {
+      id: 3,
+      titulo: "Aprende javascript",
+      lenguaje: "javascript",
+      vistas: 12500,
+      nivel: "basico",
+    },
+  ],
+  matematicas: [
+    {
+      id: 1,
+      titulo: "Aprende calculo",
+      tema: "calculo",
+      vistas: 12405,
+      nivel: "basico",
+    },
+    {
+      id: 2,
+      titulo: "Aprende algebra",
+      tema: "algebra",
+      vistas: 13223,
+      nivel: "intermedio",
+    },
+  ],
+};
+
+module.exports.infoCursos = infoCursos;
